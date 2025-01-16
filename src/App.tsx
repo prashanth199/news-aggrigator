573dd68e826f49b3ba34f1d89fb1a865b6eb4319
prashanth199 <prashanthdsouza199@gmail.com>
2025-01-17 00:29:40 +0530

    removed unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-// import Header from './components/common/Header.tsx'
-// import ArticleCard from './components/news/ArticleCard.tsx';
 import Layout from './pages/Layout/Layout.tsx';
 import HomePage from './pages/HomePage.tsx';
 import store from './redux/store.ts';
@@ -14,33 +12,6 @@ function App() {
 
   return (
     <>
-      {/* <Header onSearch={handleSearch} />
-      
-        <ArticleCard
-          title="Sample News Article"
-          description="This is a sample description of a news article. It provides a brief overview of the content."
-          imageUrl="https://picsum.photos/200"
-          sourceName="Example Source"
-          publishedAt="2025-01-15T10:00:00Z"
-          onReadMore={handleReadMore}
-        />
-        <ArticleCard
-          title="Sample News Article"
-          description="This is a sample description of a news article. It provides a brief overview of the content."
-          imageUrl="https://picsum.photos/200"
-          sourceName="Example Source"
-          publishedAt="2025-01-15T10:00:00Z"
-          onReadMore={handleReadMore}
-        />
-        <ArticleCard
-          title="Sample News Article"
-          description="This is a sample description of a news article. It provides a brief overview of the content."
-          imageUrl="https://picsum.photos/200"
-          sourceName="Example Source"
-          publishedAt="2025-01-15T10:00:00Z"
-          onReadMore={handleReadMore}
-        /> */}
-
       <BrowserRouter>
         <Provider store={store}>
           <Routes>
